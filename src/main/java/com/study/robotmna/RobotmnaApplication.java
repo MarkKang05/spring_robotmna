51e0ec04f76c9b596c30c4edd26e846dd2b27f76
macbook13 <markkang05@gmail.com>
2021-10-12 21:55:21 +0900

    source,resource files

---
+++ src/main/java/com/study/robotmna/RobotmnaApplication.java
@@ -0,0 +1,13 @@
+package com.study.robotmna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobotmnaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RobotmnaApplication.class, args);
+    }
+
+}
