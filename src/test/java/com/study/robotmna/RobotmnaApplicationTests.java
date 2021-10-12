e7e246b76c5399b200984a136753dfd179e2f1dd
macbook13 <markkang05@gmail.com>
2021-10-12 21:52:44 +0900

    Initial commit

---
+++ src/test/java/com/study/robotmna/RobotmnaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.robotmna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotmnaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
