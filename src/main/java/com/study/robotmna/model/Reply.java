5fe9aa4cc14ad573f9653d03e7efd209c813baca
MarkKang05 <markkang05@gmail.com>
2021-10-13 23:54:05 +0900

    Add Entity(user,board,reply);

---
+++ src/main/java/com/study/robotmna/model/Reply.java
@@ -0,0 +1,39 @@
+package com.study.robotmna.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Time;
+import java.sql.Timestamp;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Reply {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private int id;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "boardId")
+    private Board board;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+
+
+}
