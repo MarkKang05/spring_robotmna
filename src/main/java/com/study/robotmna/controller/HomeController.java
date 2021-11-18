b30a8e1a700bbfa3ce714b5137dd8662704f9ca0
MarkKang05 <markkang05@gmail.com>
2021-11-18 20:30:49 +0900

    header(principal.username)

--- src/main/java/com/study/robotmna/controller/HomeController.java
+++ src/main/java/com/study/robotmna/controller/HomeController.java
@@ -1,12 +1,27 @@
 package com.study.robotmna.controller;
 
+import com.study.robotmna.config.auth.PrincipalDetail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+
     @GetMapping("/")
-    public String homepage(){
+    public String homepage(@AuthenticationPrincipal PrincipalDetail principal){
+        if(principal != null){
+            System.out.println("login user: "+ principal.getUsername());
+        }
+
         return "index";
     }
+
+    @GetMapping("/board/saveForm")
+    public String saveForm(){
+        return "board/saveForm";
+    }
+
 }
